f17e9979a74b8408490856d5a7b5a241b3301fb2
Zac <poisonite@gmail.com>
2020-09-14 15:29:24 -0700

    09-14-2020 Update 1
    Completed steps 1 and 2
    partial completion on step 3

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-//step 1 select objects on screen & define globals//
+// Step 1 select objects on screen & define globals
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+const countdownBoard = document.querySelector(".countdown");
+const startButton = document.querySelector(".startButton");
 
-//step 2 add class 'up'//
+let lastHole;
+let timeUp = false;
+let score = 0;
+let countdown;
 
-//step 3 dynamically change text content to countdown//
+function pickRandomHole(holes) {
+  const randomHole = Math.floor(Math.random() * holes.length);
+  const hole = holes[randomHole];
 
-//step 4 adding event listeners//
+  if (hole === lastHole) {
+    return pickRandomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
 
-//step 5 keep track of score with forEach//
+// Step 2 add class 'up'
+function popOut() {
+  const time = Math.random() * 1300 + 400;
+  const hole = pickRandomHole(holes);
+
+  hole.classList.add("up");
+  setTimeout(function() {
+    hole.classList.remove("up");
+    if (!timeUp) {
+      popOut();
+    }
+  }, time);
+}
+
+// Step 3 dynamically change text content to countdown
+function startGame() {
+  countdown = timeLimit / 20;
+  scoreBoard.textContent = 0;
+  scoreBoard.style.display = "block";
+  countdownBoard.textContent = countdown;
+  timeUp = false;
+  score = 0;
+  popOut();
+
+  setTimeout(function() {
+    timeUp = true;
+  }, timeLimit);
+
+  let startCountdown = setInterval();
+}
+
+// Step 4 adding event listeners
+
+// Step 5 keep track of score with forEach
