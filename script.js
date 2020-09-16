e152dc8993bc7f483318c6dc2a7446281cdf4dc1
Zac <poisonite@gmail.com>
2020-09-16 13:51:13 -0700

    09-16-2020 Update 1
    Finished steps as described

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ let lastHole;
 let timeUp = false;
 let score = 0;
 let countdown;
+let timeLimit = 20000;
 
 function pickRandomHole(holes) {
   const randomHole = Math.floor(Math.random() * holes.length);
+  // console.log(`Fired with time: ${randomHole}`);
   const hole = holes[randomHole];
 
   if (hole === lastHole) {
@@ -24,16 +26,21 @@ function pickRandomHole(holes) {
 // Step 2 add class 'up'
 function popOut() {
   const time = Math.random() * 1300 + 400;
+  // console.log(`Fired with PopOut time: ${time}`);
   const hole = pickRandomHole(holes);
+  console.log(hole);
 
   hole.classList.add("up");
   setTimeout(function() {
     hole.classList.remove("up");
     if (!timeUp) {
+      // console.log(`Time Up is false!`);
       popOut();
     }
   }, time);
 }
+
+window.onload = startGame();
 
 // Step 3 dynamically change text content to countdown
 function startGame() {
@@ -43,15 +50,39 @@ function startGame() {
   countdownBoard.textContent = countdown;
   timeUp = false;
   score = 0;
+  console.log(countdown);
   popOut();
 
   setTimeout(function() {
     timeUp = true;
   }, timeLimit);
 
-  let startCountdown = setInterval();
+  let startCountdown = setInterval(function() {
+    countdown -= 1;
+    countdownBoard.textContent = countdown;
+    if (countdown < 0) {
+      countdown = 0;
+      clearInterval(startCountdown);
+      countdownBoard.textContent =
+        "Time is up!! Thank you for protecting our planet. This is the way!";
+    }
+  }, 1000);
 }
 
 // Step 4 adding event listeners
+startButton.addEventListener("click", startGame);
+function whack(e) {
+  score++;
+  this.style.backgroundImage = 'url("yoda2.png")';
+  this.style.pointerEvents = "none";
+
+  setTimeout(() => {
+    this.style.backgroundImage = 'url("yoda1.png")';
+    this.style.pointerEvents = "all";
+  }, 800);
+
+  scoreBoard.textContent = score;
+}
 
 // Step 5 keep track of score with forEach
+moles.forEach(mole => mole.addEventListener("click", whack));
